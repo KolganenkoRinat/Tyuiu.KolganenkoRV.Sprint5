3b46a9bdebbea0009c459b410597215ffac3560d
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-11-14 22:08:28 +0500

    Task0

--- Tyuiu.KolganenkoRV.Sprint5.Task0.V5.Lib/DataService.cs
+++ Tyuiu.KolganenkoRV.Sprint5.Task0.V5.Lib/DataService.cs
@@ -8,7 +8,8 @@ namespace Tyuiu.KolganenkoRV.Sprint5.Task0.V5.Lib
         public string SaveToFileTextData(int x)
         {
             string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double z = Math.Log((x + 1) / (x + 2));
+            double z;
+            z = Math.Round(Math.Log((x + 1) / (x + 2)), 3);
             File.WriteAllText(path, Convert.ToString(z));
             return path;
         }
