bf9c45c3d176b89dd29fec4c7dbf270a46a1d256
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-11-20 17:16:21 +0500

    Task4

---
+++ Tyuiu.KolganenkoRV.Sprint5.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KolganenkoRV.Sprint5.Task4.V17.Lib
+{
+    public class DataService : ISprint5Task4V17
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(Math.Sin(2.0 / 3.0 * Convert.ToDouble(strX)) + Math.Pow(Convert.ToDouble(strX), 2), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint5.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KolganenkoRV.Sprint5.Task4.V17.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint5.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Ринат\DataSprint5\InPutDataFileTask4V17.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
